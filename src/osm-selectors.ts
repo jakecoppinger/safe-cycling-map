8f4446b4622678b6ae7d0bae5730fba2d10fcac7
Jake Coppinger <jake@jakecoppinger.com>
2023-01-28 13:11:46 +1100

    Refactor road selectors

--- src/osm-selectors.ts
+++ src/osm-selectors.ts
@@ -1,20 +1,27 @@
-import { FeatureCollection, Geometry, GeoJsonProperties, Feature, GeometryObject } from 'geojson';
+import { Geometry, GeoJsonProperties, Feature } from 'geojson';
 
-export function isRedRoad(feature: Feature<Geometry, GeoJsonProperties>) {
-  return feature.properties &&
-    (feature.properties.maxspeed > 40 || (feature.properties.highway === 'residential' && feature.properties.maxspeed === undefined))
+export function isRedRoad(feature: Feature<Geometry, GeoJsonProperties>): boolean {
+  if (feature.properties === null) {
+    return false;
+  }
+  return (feature.properties.maxspeed > 40 || (feature.properties.highway === 'residential' && feature.properties.maxspeed === undefined))
 }
 
-export function isOrangeRoad(feature: Feature<Geometry, GeoJsonProperties>) {
-  return feature.properties &&
-    (feature.properties.maxspeed <= 40 || feature.properties.cycleway === 'lane')
+export function isOrangeRoad(feature: Feature<Geometry, GeoJsonProperties>): boolean {
+  if (feature.properties === null) {
+    return false;
+  }
+
+  return (feature.properties.maxspeed <= 40 || feature.properties.cycleway === 'lane')
 
 }
-export function isGreenRoad(feature: Feature<Geometry, GeoJsonProperties>) {
-  return feature.properties &&
-    (feature.properties.maxspeed <= 30 || feature.properties.highway === 'cycleway' || feature.properties.highway === 'pedestrian'
+export function isGreenRoad(feature: Feature<Geometry, GeoJsonProperties>): boolean {
+  if (feature.properties === null) {
+    return false;
+  }
+  return (feature.properties.maxspeed <= 30 || feature.properties.highway === 'cycleway' || feature.properties.highway === 'pedestrian'
 
-      || feature.properties.highway === 'living_street'
-    )
+    || feature.properties.highway === 'living_street'
+  )
 
 }
